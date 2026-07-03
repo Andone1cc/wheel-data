8e6cb6709845bf4727c7c511cd5920cac8d27237
Andone1cc <andone1cc@gmail.com>
2026-07-03 11:21:52 +0800

    Add files via upload

---
+++ api/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        amber: '#f5b731',
+        profit: '#3dd68c',
+        loss: '#f46a5a',
+        blue: '#4fa3e0',
+        teal: '#2dd4bf',
+      },
+    },
+  },
+  plugins: [],
+};
